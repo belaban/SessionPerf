d1796fe98e4cc0a8b12011d60f4aa7e34b7a9a6f
Bela Ban <belaban@yahoo.com>
2012-05-15 15:24:19 +0200

    ns

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,9 @@ for server in $SERVERS
 do
     echo "Deploying $1 into ${PREFIX}$server:/tmp/standalone/deployments"
 
-    scp $1 $server:/tmp/standalone/
-    ssh $server "find /tmp/standalone -name deployments -exec cp /tmp/standalone/`basename $1` {}/ \;"
-    ssh $server "rm -f /tmp/standalone/`basename $1`"
+    scp $1 ${PREFIX}$server:/tmp/standalone/
+    ssh ${PREFIX}$server "find /tmp/standalone -name deployments -exec cp /tmp/standalone/`basename $1` {}/ \;"
+    ssh ${PREFIX}$server "rm -f /tmp/standalone/`basename $1`"
 done
 
 
